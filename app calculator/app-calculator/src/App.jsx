dc3bb9f90f6357ab394e038da4009a71c78eb0ef
Rafael Wolfart <rafaelwolfart1@outlook.com>
2023-02-04 17:13:44 -0300

    Calculator v1.0

---
+++ app calculator/app-calculator/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+import Keypad from './Components/Keypad'; 
+
+function App() {
+    const [input, setInput] = useState('');
+    const handleClick = (value) => {
+      setInput(input + value);
+    };
+
+    const handleEqual = () => {
+      setInput(eval(input).toString());
+    };
+
+    const handleClear = () => {
+      setInput("");
+    };
+    return (
+      <div className='App'>
+        <div className='Calculator'>
+          <input className='display' type="text" value={input} readOnly/>
+          <Keypad 
+          handleClear={handleClear}
+          handleClick={handleClick}
+          handleEqual={handleEqual}/>
+        </div>
+      </div>
+    );
+};
+
+export default App
